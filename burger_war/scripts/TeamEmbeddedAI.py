7237326a342aea0d3846eb1e67aa3de31bbe91be
kyad <53517611+kyad@users.noreply.github.com>
2020-08-09 01:17:59 +0900

    Prepare for competing against myself

--- burger_war/scripts/TeamEmbeddedAI.py
+++ burger_war/scripts/TeamEmbeddedAI.py
@@ -311,10 +311,9 @@ class RandomBot():
         rot     = get_rotation_matrix(45 * np.pi / 180)                         # 45度回転行列の定義
         desti   = np.dot(rot, pos)                                              # 45度回転
         yaw = np.arctan2( (desti[1]-self.pos[1]), (desti[0]-self.pos[0]) )      # 移動先の角度
-        if self.my_color == 'r' :
-            #print('****Action****', self.timer, action, desti, yaw*360/np.pi)
-            print('* Action * Time=%2d : %4.2f,  Score=(%2d,%2d), Position=(%4.2f, %4.2f),  Destination=(%4.2f, %4.2f, %4.0f[deg])' % (self.timer, self.time, self.score[0], self.score[1], self.pos[0], self.pos[1], desti[0], desti[1], yaw*360/np.pi))
-            print('')
+        #print('****Action****', self.timer, action, desti, yaw*360/np.pi)
+        print(self.my_color, '* Action * Time=%2d : %4.2f,  Score=(%2d,%2d), Position=(%4.2f, %4.2f),  Destination=(%4.2f, %4.2f, %4.0f[deg])' % (self.timer, self.time, self.score[0], self.score[1], self.pos[0], self.pos[1], desti[0], desti[1], yaw*360/np.pi))
+        print('')
         
         # Actionに従った行動  目的地の設定 (X, Y, Yaw)
         self.setGoal(desti[0], desti[1], yaw)
@@ -549,8 +548,12 @@ if __name__ == '__main__':
     #   ・確率でのランダム動作を行わない
     Sim_flag = True
     
-    rname = rosparam.get_param('randomRun/rname')
-    rside = rosparam.get_param('randomRun/rname')
+    try:
+        rname = rosparam.get_param('randomRun/rname')
+        rside = rosparam.get_param('randomRun/rname')
+    except:
+        rname = 'b'
+        rside = 'b'
     if rname == 'red_bot' or rside == 'r': color = 'r'
     else                                 : color = 'b'
     print('****************', rname, rside, color)
